7fdf685fa358b20c7b669d058ef03ab2db9f7249
AJLang53 <AJLang53@hotmail.com>
2017-06-08 15:19:34 -0400

    Organizes rx input based on constructor arguments

--- ir_rx.py
+++ ir_rx.py
@@ -2,41 +2,90 @@ import pigpio
 
 class rx:
 
-    def __init__(self, pi, gpio, dig1, dig0, startBit, totalBits):
+    def __init__(self, pi, gpio, dig1, dig0, startBit, dataBits,addressBits):
+        """
+        Constructor. Sets the GPIO pin to input mode, and connects callbacks to rising and falling edge events
+        """
         self.pi = pi
         self.gpio = gpio
-        self.dig1 = dig1
-        self.dig0 = dig0
-        self.startBit = startBit
-        self.totalBits = totalBits
-        self.high = False
+        self.dig1 = dig1    # Length of digital 1
+        self.dig0 = dig0    # Length of digital 0
+        self.startBit = startBit    # Length of start bit
+        self.dataBits = dataBits    # Total number of data bits
+        self.addressBits = addressBits  # Total number of address bits
         
+        self.tol = 100      # 100 microsecond tolerance
+        self.started = False
+        self.high = False
+        self.riseTime = 0
         self.transmissions = []
         self.bits = []
-        self.curBit = []
         
         self.pi.set_mode(self.gpio, pigpio.INPUT)
         cb_rising = self.pi.callback(self.gpio, pigpio.RISING_EDGE, self.rising)
         cb_falling = self.pi.callback(self.gpio, pigpio.FALLING_EDGE, self.falling)
         
     def rising(self, gpio, level, tick):
+        """
+        Callback function for rising edge
+        """
+        
         if not self.high:
-            if len(curBit) != 0:
-                self.curBit[2] = self.pi.get_current_tick()
-                self.bits.append(self.curBit)
-                self.curBit = []
-                self.curBit[0] = tick
-            else:
-                self.curBit[0] = tick
+            self.riseTime = tick
+            
         self.high = True
         
-    def falling(self):
-        if self.high:
-            self.curBit[1] = self.pi.get_current_tick()
+    def falling(self, gpio, level, tick):
+        """
+        Callback function for falling edge
+        """
+        
+        # If there was a previous rising edge, but no start bit, check for a start bit
+        if self.high and not self.started:
+            timeHigh = pigpio.tickDiff(self.riseTime, tick)
+            if abs(timeHigh - self.startBit) < tol:
+                self.started = True
+        
+        # If there was a previous rising edge and start bit, determine if it's a 1 or 0
+        elif self.high and self.started:
+            timeHigh = pigprio.tickDiff(self.riseTime, tick)
+            if abs(timeHigh - self.dig1) < tol:
+                self.bits.append('1')
+            else
+                self.bits.append('0')
+                
+            # If all of the bits are received, add it to transmissions and reset bits
+            if len(self.bits) == (self.dataBits + self.addressBits)
+                self.transmissions.append(self.bits)
+                self.bits = []
+                
+        self.high = False
             
     def received(self):
-        return self.received
+        """
+        Return transmission state
+        """
+        
+        if len(self.transmissions) != 0:
+            return True
+        else
+            return False
     
     def getTransmission(self):
-        return self.bits
+        """
+        Return the oldest transmission as a tuple of (data, address) and removes it
+        """
         
+        trans = self.transmissions.pop(0)
+        transData = trans[0:dataBits]
+        transAddress = trans[dataBits:]
+            
+        return (transData, transAddress)
+    
+    def end(self):
+        """
+        Ends the pi connection
+        """
+        
+        self.pi.stop()
+        return
