5d3076747e3c08f0e5bda6ab3015db86c92ef81e
AJLang53 <AJLang53@hotmail.com>
2017-06-08 11:12:09 -0400

    Removed Example

--- tx.py
+++ tx.py
@@ -44,35 +44,3 @@ class tx:
 
         # add off cycles of no carrier
         self.wf.append(pigpio.pulse(0, 0, off * self.micros))
-
-if __name__ == "__main__":
-
-   import time
-   import pigpio
-   import ir_tx
-
-   pi = pigpio.pi()
-
-   tx = ir_tx.tx(pi, 22, 38000)
-
-   tx.clear_code()
-
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 46) # 1
-   tx.add_to_code(23, 23) # 0
-   tx.add_to_code(23, 23) # 0
-
-   tx.send_code()
-   tx.clear_code()
-
-   pi.stop()
