312152aec6aecc29cb94de6c32ab16533ec8f639
AJLang53 <AJLang53@hotmail.com>
2017-06-08 13:28:53 -0400

    Working on handling transmission

--- ir_rx.py
+++ ir_rx.py
@@ -1,13 +1,17 @@
 import pigpio
-import time
 
 class rx:
 
-    def __init__(self, pi, gpio):
+    def __init__(self, pi, gpio, dig1, dig0, startBit, totalBits):
         self.pi = pi
         self.gpio = gpio
+        self.dig1 = dig1
+        self.dig0 = dig0
+        self.startBit = startBit
+        self.totalBits = totalBits
         self.high = False
         
+        self.transmissions = []
         self.bits = []
         self.curBit = []
         
@@ -15,17 +19,24 @@ class rx:
         cb_rising = self.pi.callback(self.gpio, pigpio.RISING_EDGE, self.rising)
         cb_falling = self.pi.callback(self.gpio, pigpio.FALLING_EDGE, self.falling)
         
-    def rising(self):
+    def rising(self, gpio, level, tick):
         if not self.high:
             if len(curBit) != 0:
                 self.curBit[2] = self.pi.get_current_tick()
                 self.bits.append(self.curBit)
                 self.curBit = []
+                self.curBit[0] = tick
             else:
-                self.curBit[0] = self.pi.get_current_tick()
+                self.curBit[0] = tick
         self.high = True
         
     def falling(self):
         if self.high:
             self.curBit[1] = self.pi.get_current_tick()
+            
+    def received(self):
+        return self.received
+    
+    def getTransmission(self):
+        return self.bits
         
