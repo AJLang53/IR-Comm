02d309479fa5134036117c83929abe05fa57351f
AJLang53 <AJLang53@hotmail.com>
2017-06-08 15:29:36 -0400

    Create rx_example.py

---
+++ rx_example.py
@@ -0,0 +1,15 @@
+import pigpio
+import ir_rx
+
+pi = pigpio.pi()
+
+rx = ir_rx.rx(pi, 14, 1200, 600, 2400, 7, 5)  # SIRC protocol
+
+while not rx.received:
+    pass
+    
+trans = rx.getTransmission()
+print("Data",trans[0])
+print("Address",trans[1])
+
+pi.stop()
