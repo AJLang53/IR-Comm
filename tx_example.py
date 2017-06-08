2c77b8200db3fdbbae31d7309d2a6bf57d70b373
AJLang53 <AJLang53@hotmail.com>
2017-06-08 15:39:14 -0400

    Updated tx_example to match rx_example

--- tx_example.py
+++ tx_example.py
@@ -8,19 +8,19 @@ tx = ir_tx.tx(pi, 22, 38000)
 
 tx.clear_code()
 
-tx.add_to_code(46, 11) # Start
-tx.add_to_code(23, 11) # 1
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(23, 11) # 1
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(23, 11) # 1
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(23, 11) # 1
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(11, 11) # 0
-tx.add_to_code(23, 11) # 1
-tx.add_to_code(11, 11) # 0
+tx.add_to_code(92, 23) # Start
+tx.add_to_code(46, 23) # 1
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(46, 23) # 1
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(46, 23) # 1
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(46, 23) # 1
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(23, 23) # 0
+tx.add_to_code(46, 23) # 1
+tx.add_to_code(23, 23) # 0
 
 tx.send_code()
 tx.clear_code()
