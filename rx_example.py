7bfd431a29dd4342a627cdf434da4c7133cf5196
AJLang53 <AJLang53@hotmail.com>
2017-06-14 15:13:10 -0400

    Updated for threading

--- rx_example.py
+++ rx_example.py
@@ -1,15 +1,19 @@
 import pigpio
+import threading, Queue
 import ir_rx
 
 pi = pigpio.pi()
+transmissions = Queue.LifoQueue()
+interrupt =  Queue.Queue()
+rx = ir_rx.rx_pigpio(pi, 14, transmissions, interrupt, 1200, 600, 2400, 7, 5)  # SIRC protocol
+rx.start()
 
-rx = ir_rx.rx_pigpio(pi, 14, 1200, 600, 2400, 7, 5)  # SIRC protocol
-
-while not rx.received:
+while self.transmissions.empty():
     pass
     
-trans = rx.getTransmission()
+trans = self.transmissions.get()
 print("Data",trans[0])
 print("Address",trans[1])
 
+self.interrupt.put("stop")
 pi.stop()
