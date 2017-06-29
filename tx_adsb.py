c24f3f9aeef8d44e12b3ea8f00106f40f36cf532
Austin <AJLang53@gmail.com>
2017-06-29 21:28:22 +0000

    Better ADS-B Transmitting

--- tx_adsb.py
+++ tx_adsb.py
@@ -2,6 +2,7 @@ import time, math
 import pigpio
 import ir_tx
 from CPR import CPR
+import argparse
 
 def crc(msg, encode = False):
     GENERATOR = '1111111111111010000001001'
@@ -18,91 +19,127 @@ def crc(msg, encode = False):
     reminder = ''.join(msgbin[-24:])
     return reminder
 
+if __name__ == '__main__':
     
-# Transmit Parameters
-carrierHz = 38000
-bitSize = 225   # The time size of the on cycle
-pulseNum = bitSize/(1000000/carrierHz)  # Number of cycles of carrier to fill bitSize
-gpio = 18
+    # Transmit Parameters
+    carrierHz = 38000
+    bitSize = 225   # The time size of the on cycle
+    pulseNum = bitSize/(1000000/carrierHz)  # Number of cycles of carrier to fill bitSize
+    gpio = 18
 
 
-# Create the data format and emitter category binary
-DF = 17
-DFBin = bin(DF)[2:]
-while len(DFBin)<5:
-    DFBin = '0'+DFBin
+    # Create the data format and emitter category binary
+    DF = 17
+    DFBin = bin(DF)[2:]
+    while len(DFBin)<5:
+        DFBin = '0'+DFBin
+    EC = 5
+    ECBin = bin(EC)[2:]
+    while len(ECBin)<3:
+        ECBin = '0'+ECBin
 
-EC = 5
-ECBin = bin(EC)[2:]
-while len(ECBin)<3:
-    ECBin = '0'+ECBin
+    # Create the ICAO24 binary (unique aircraft ID)
+    ICAO24 = '101010101010101010101010'
 
-# Create the ICAO24 binary (unique aircraft ID)
-ICAO24 = '101010101010101010101010'
+    # Create the preamble binary
+    preamble = DFBin+ECBin+ICAO24
 
-# Create the preamble binary
-preamble = DFBin+ECBin+ICAO24
+    # Location Information
+    altBase = 25
+    NZ = 15
+    lat = float(raw_input("Latitude: "))
+    lon = float(raw_input("Longitude: "))
+    alt = float(raw_input("Altitude: "))
 
-# Location Information
-altBase = 25
-NZ = 15
-lat = float(raw_input("Latitude: "))
-lon = float(raw_input("Longitude: "))
-alt = float(raw_input("Altitude: "))
+    # Create the binary lat and lon in CPR format
+    cpr = CPR()
+    evenLoc = cpr.encode(lat, lon, 0)
+    evenLat = bin(evenLoc[0])[2:]
+    evenLon = bin(evenLoc[1])[2:]
+    oddLoc = cpr.encode(lat, lon, 1)
+    oddLat = bin(oddLoc[0])[2:]
+    oddLon = bin(oddLoc[1])[2:]
 
-# Create the binary lat and lon in CPR format
-cpr = CPR()
-evenLoc = bin(cpr.encode(lat, lon, 0))[2:]
-oddLoc = bin(cpr.encode(lat, lon, 1))[2:]
+    while len(evenLat)<17:
+        evenLat = '0'+evenLat
+    while len(evenLon)<17:
+        evenLon = '0'+evenLon
+    while len(oddLat)<17:
+        oddLat = '0'+oddLat
+    while len(oddLon)<17:
+        oddLon = '0'+oddLon
 
-# Create the binary altitude in CPR format
-cprAlt = cpr.encodeAlt(alt, altBase)
-    
-# Create the Type code, surveillance status, NIC supplement-B, Time, and CPR Odd/Even binary
-TC = bin(11)[2:]
-while len(TC)<5:o
-    TC = '0'+TC
-SS = bin(0)[2:]
-while len(SS)<2:
-    SS = '0'+SS
-NICsB = bin(0)[2:]
-T = bin(0)[2:]
-F = bin(0)[2:]
+    # Create the binary altitude in CPR format
+    cprAlt = cpr.encodeAlt(alt, altBase)
+        
+    # Create the Type code, surveillance status, NIC supplement-B, and Time binary
+    TC = bin(11)[2:]
+    while len(TC)<5:
+        TC = '0'+TC
+    SS = bin(0)[2:]
+    while len(SS)<2:
+        SS = '0'+SS
+    NICsB = bin(0)[2:]
+    T = bin(0)[2:]
 
-# Assemble the data binary (56 bits)
-dataBin = TC+SS+NICsB+altCPR+T+F+YZBin+XZBin
+    # Assemble the data binary (56 bits)
+    evenDataBin = TC+SS+NICsB+cprAlt+T+'0'+evenLat+evenLon
+    oddDataBin = TC+SS+NICsB+cprAlt+T+'1'+oddLat+oddLon
 
-# Create the parity and add it to the message
-parityBin = crc(preamble+dataBin)
-print(parityBin)
-trans = preamble+dataBin+parityBin
+    # Create the parity and add it to the message
+    evenParityBin = crc(preamble+evenDataBin)
+    oddParityBin = crc(preamble+oddDataBin)
+    evenTrans = preamble+evenDataBin+evenParityBin
+    oddTrans = preamble+oddDataBin+oddParityBin
 
-print("ADS-B Transmission: "+trans)
-print('\n')
+    while len(evenTrans)<112:
+        evenTrans = '0'+evenTrans
+    while len(oddTrans)<112:
+        oddTrans = '0'+oddTrans
 
-# Set up for transmitting
-pi = pigpio.pi()
-tx = ir_tx.tx(pi, gpio, carrierHz)
+    print("ADS-B Even Transmission: "+evenTrans)
+    print("ADS-B Odd Transmission: "+oddTrans)
+    print('\n')
 
-while len(trans)<112:
-    trans = '0'+trans
+    # Set up for transmitting
+    pi = pigpio.pi()
+    tx = ir_tx.tx(pi, gpio, carrierHz)
 
-# Create and Send the message
-tx.clear_code()
-tx.add_to_wave(pulseNum, pulseNum)
+    # Create and begin sending
+    try:
+        while True:
+            tx.clear_code()
+            tx.add_to_wave(pulseNum, pulseNum)
 
-k=0
-while k< len(trans):
-    if trans[k] == '1':
-        tx.add_to_wave(pulseNum, pulseNum)
-    elif trans[k] == '0':
-        tx.add_to_wave(0, pulseNum)
-        tx.add_to_wave(pulseNum, 0)
-    k+=1
+            k=0
+            while k < len(evenTrans):
+                if evenTrans[k] == '1':
+                    tx.add_to_wave(pulseNum, pulseNum)
+                elif evenTrans[k] == '0':
+                    tx.add_to_wave(0, pulseNum)
+                    tx.add_to_wave(pulseNum, 0)
+                k+=1
 
-tx.send_wave()
+            tx.send_wave()
+            
+            tx.clear_code()
 
-tx.clear_code()
 
-pi.stop()
-print("Finished")
+            time.sleep(0.1) # 100 ms between even and odd tranmissions
+
+            k=0
+            while k < len(oddTrans):
+                if oddTrans[k] == '1':
+                    tx.add_to_wave(pulseNum, pulseNum)
+                elif oddTrans[k] == '0':
+                    tx.add_to_wave(0, pulseNum)
+                    tx.add_to_wave(pulseNum, 0)
+                k+=1
+
+            tx.send_wave()
+
+            time.sleep(0.1)
+            
+    except KeyboardInterrupt:
+        pi.stop()
+        print("Stopped")
